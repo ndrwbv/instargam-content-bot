d8cc942d1a4885f50b6e5d504b633d43e7b4c349
Andrew <ndrwbv@gmail.com>
2019-01-15 21:28:53 +0700

    right path's

--- test_make.sh
+++ test_make.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-node unsplash_get.js
-convert pic.png -strokewidth 0 -fill "rgba( 0, 0, 0 , 0.5 )" -draw "rectangle 1080,0 0,1080 " pic-o.png 
-convert pic-o.png -size 1080x1080  -gravity West   \
-            -font ~/Downloads/Roboto/Roboto-Light.ttf -stroke  none -fill white -pointsize 40  -annotate +180-446   "$(head -1 author.txt)"  \
-            -font ~/Downloads/Roboto/Roboto-Italic.ttf -stroke  none -fill white -pointsize 30  -annotate +180-398  "$(tail -n  +2 author.txt )"  \
+node js/unsplash_get.js
+convert img/pic-source.png -strokewidth 0 -fill "rgba( 0, 0, 0 , 0.5 )" -draw "rectangle 1080,0 0,1080 " img/pic-1.png 
+convert img/pic-1.png -size 1080x1080  -gravity West   \
+            -font ~/Downloads/Roboto/Roboto-Light.ttf -stroke  none -fill white -pointsize 40  -annotate +180-446   "$(head -1 resources/author.txt)"  \
+            -font ~/Downloads/Roboto/Roboto-Italic.ttf -stroke  none -fill white -pointsize 30  -annotate +180-398  "$(tail -n  +2 resources/author.txt )"  \
             -gravity Center \
-           -font ~/Downloads/Roboto/Roboto-Medium.ttf -stroke  none -fill white -pointsize 70  -annotate +1+20   "$(cat quote.txt)"  \
+           -font ~/Downloads/Roboto/Roboto-Medium.ttf -stroke  none -fill white -pointsize 70  -annotate +1+20   "$(cat resources/quote.txt)"  \
            -font ~/Downloads/Roboto/Roboto-Thin.ttf -stroke  none -fill white -pointsize 30  -annotate +1+500   "@bot_keklik"  \
-          pic_ready.png 
-
-composite -geometry  +87+78 Rocket.png pic_ready.png  pic_ready_2.png 
+          img/pic-2.png 
+composite -geometry  +87+78 assets/rocket.png pic_ready.png  img/pic-ready.png 
