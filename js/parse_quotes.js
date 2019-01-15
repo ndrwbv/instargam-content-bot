0521561e2f4196a7f33b46b81aeeb0c0724eee76
Andrew <ndrwbv@gmail.com>
2019-01-16 01:56:27 +0700

    дробление текста и запись в файлы

--- js/parse_quotes.js
+++ js/parse_quotes.js
@@ -1,43 +1,89 @@
 var request = require('request');
-var iconv  = require('iconv-lite');
+var iconv = require('iconv-lite');
 
-//var fs = require('fs');
+var fs = require('fs');
 var cheerio = require('cheerio');
 
 var opt = {
-    url: 'https://www.brainyquote.com/topics/celebrity',
-    encoding: null
+  url: 'https://www.brainyquote.com/topics/celebrity',
+  encoding: null
 }
 
 var authors = [];
 var quotes = [];
-function parseQuots(){
+
+function parseQuots() {
   request(opt, function (err, res, body) {
     if (err) throw err;
     var $ = cheerio.load(iconv.decode(body, 'utf8'));
     //bq-aut b-qt
 
-    $(".bq-aut").each(function(i, elem) {
-      authors.push( $(this).text() );
+    $(".bq-aut").each(function (i, elem) {
+      authors.push($(this).text());
     });
-    $(".b-qt").each(function(i, elem) {
-      quotes.push( $(this).text() );
+    $(".b-qt").each(function (i, elem) {
+      quotes.push($(this).text());
     });
   });
+
+  //make quotes in json
+  //"0" { 'quote': 'Lorem','author':'John, Coraline' } 
 }
-function getQuote(){
-  //create and write in file quoute.txt one quoute with \n
-  for(let i in authors){
-    //console.log({aut: authors[i], quot: quotes[i]});
-    console.log(quotes[i] + '\n\n' + authors[i]);
-    return 0;
-  } 
+function makeJson() {
+  return json;
 }
-//длина 18 M значит 9 символов в строке должно быть
-if(!empty(file)) getQuote();
-else{
-  //promise or set timeout 
-  parseQuots();
-  getQuote();
-} 
+
+function makePadding(source_string) {
+  let words_counter = 0;
+  let indexes = [];
+  indexes.push(0);
+  for (let i in source_string) {
+    words_counter++;
+    if (words_counter == 27) {
+      for (let k = i; k < source_string.length; k++) {
+        if (source_string[k] == ' ') {
+          indexes.push(k);
+          break;
+        }
+      }
+      words_counter = 0;
+    }
+  }
+  let padding_string = "";
+  for (let i = 0; i < indexes.length; i++) {
+    padding_string += source_string.substring(indexes[i], indexes[i+1]) + "\n";
+  }
+
+  return padding_string.replace(/\n /g ,"\n");
+}
+
+function writeIn(path, data) {
+  fs.writeFile(path, data, function(err){
+    if(err) throw err;
+    console.log("written: " + path);
+  });
+}
+function getQuote() {
+  //parse quotes json
+  //get first element
+  //write it in resources/quote.txt and in resources/author.txt
+  let _author = "Neil Gaiman\nCoraline"
+  let _quote = "“Fairy tales are more than true: not because they tell us that dragons exist, but because they tell us that dragons can be beaten.”";
+
+  writeIn('resources/quote.txt', makePadding(_quote));
+  writeIn('resources/author.txt', _author);
+  
+  //remove first emlement 
+  //write new json
+}
+
+getQuote();
+// if(!empty(file)) getQuote();
+// else{
+//   //promise or set timeout 
+//   parseQuots();
+//   getQuote();
+// } 
+
+
 
