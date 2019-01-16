39c96e6edfa3334d94a6da7bed94cb4adc0b18db
Andrew <ndrwbv@gmail.com>
2019-01-16 13:26:02 +0700

    закончил парсер

--- js/parse_quotes.js
+++ js/parse_quotes.js
@@ -1,11 +1,11 @@
 var request = require('request');
 var iconv = require('iconv-lite');
-
 var fs = require('fs');
 var cheerio = require('cheerio');
 
 var opt = {
-  url: 'https://www.goodreads.com/quotes?page=100',
+  url: 'https://www.goodreads.com/quotes?page=',
+  page_number: 0,
   encoding: null
 }
 
@@ -13,21 +13,26 @@ var authors = [];
 var quotes = [];
 
 function parseQuots() {
-  request(opt, function (err, res, body) {
-    if (err) throw err;
-    var $ = cheerio.load(iconv.decode(body, 'utf8'));
-
-    $(".quoteText").each(function (i, elem) {
-     let array = $(this).text().replace(/\s{2,}/g, ' ').split("”");
-     quotes.push(array[0].replace(" “",""));
-     authors.push(array[1].replace(" ― ", "").split(', ').join("\n"));
+  return new Promise(function(resolve, reject){
+    console.log("Start parsing..");
+    request(opt, function (err, res, body) {
+      
+      if (err) reject(err);
+      var $ = cheerio.load(iconv.decode(body, 'utf8'));
+  
+      $(".quoteText").each(function (i, elem) {
+       let array = $(this).text().replace(/\s{2,}/g, ' ').split("”");
+       quotes.push(array[0].replace(" “",""));
+       authors.push(array[1].replace(" ― ", "").split(', ').join("\n"));
+      });
+      
+      resolve(makeJson(quotes));
+  
     });
-    
-    writeIn('resources/quotes.json',makeJson());
   });
-
 }
-function makeJson() {
+function makeJson(quotes) {
+  console.log("Making json..");
   var quotes_json = [];
   for(q in quotes)
   {
@@ -37,7 +42,6 @@ function makeJson() {
     };
     quotes_json.push(obj);
   }
-
   return JSON.stringify(quotes_json, null, 2);
 }
 
@@ -73,33 +77,50 @@ function writeIn(path, data) {
   });
 }
 
-function getQuote() {
-
+function getFirstQuote(quotes){
+  console.log("Getting first quote..");
   let _author = ""
   let _quote = "";
-  //promise
-  fs.readFile('resources/quotes.json', (err, data) => {  
-    if (err) throw err;
-    let quotes = JSON.parse(data);
-    
-    //тут точно промис надо
-    if(typeof quotes !== 'undefined' && quotes.length > 0){
-      parseQuots();
-    }
-    for(let i in quotes ){
-      _quote = quotes[i].quote;
-      _author = quotes[i].author;
-      quotes.splice(0,1);
-      break;
-    }
-    let json = JSON.stringify(quotes, null, 2)
-    writeIn('resources/quotes.json',json);
 
-    writeIn('resources/quote.txt', makePadding(_quote));
-    writeIn('resources/author.txt', _author);
-  });
-  
+  for(let i in quotes ){
+    _quote = quotes[i].quote;
+    _author = quotes[i].author;
+    quotes.splice(0,1);
+    break;
+  }
+  let json = JSON.stringify(quotes, null, 2);
+
+  writeIn('resources/quotes.json',json);
+  writeIn('resources/quote.txt', makePadding(_quote));
+  writeIn('resources/author.txt', _author);
+
 }
 
-getQuote();
+fs.readFile('resources/quotes.json', (err, data) => {  
+  console.log("Reading resources/quotes.json");
+  if (err) throw err;
+  let quotes = JSON.parse(data);
+
+  if(quotes.length == 0)
+  {
+    console.log("Json is empty");
+    fs.readFile('assets/page_number.txt',(err, data) => {
+      opt.page_number = Number(data);
+    
+      opt.url += opt.page_number;
+      console.log("Url: " + opt.url);
+
+      parseQuots()
+      .then(
+        result => {
+          getFirstQuote(JSON.parse(result));
+          writeIn('assets/page_number.txt', opt.page_number-1);
+        }
+      )
+      .catch(err => console.error(err));
+    })
+      
+  }
+  else getFirstQuote(quotes);
+});
 
