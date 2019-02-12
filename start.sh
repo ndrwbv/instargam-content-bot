441639f1fb592324a5ded34fcadeb7848cfc7bdf
Andrew <ndrwbv@gmail.com>
2019-02-12 20:43:18 +0700

    make log better

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 echo $(date)
 node=`which node`;
-full=``; # absolute path to script folder. 
+full=`pwd`; # change to absolute path to sh folder. Ex. "/Users/andrew/Desktop/insta-project/bot_keklik/" 
 convert=`which convert`;
 composite=`which composite`;
 
 $node $full/js/unsplash_get.js
 $convert $full/img/pic-source.png -strokewidth 0 -fill "rgba( 0, 0, 0 , 0.5 )" -draw "rectangle 1080,0 0,1080 " $full/img/pic-1.png
-echo 'Overlay done.'
+echo '  sh:out Overlay done.'
 
-$node $full/js/parse_quotes.js
+$node $full/js/index.js
 a=`head -1 $full/resources/is_long.txt`;
 if (($a == 1)); then
  $convert $full/img/pic-1.png -size 1080x1080  -gravity Center   \
@@ -18,22 +18,20 @@ if (($a == 1)); then
             -gravity Center \
            -font $full/resources/Roboto/Roboto-Thin.ttf -stroke  none -fill white -pointsize 30  -annotate +1+500   "@bot_keklik"  \
           $full/img/pic-2.png
- echo 'Long Quote + author done.'
  $composite -gravity Center -geometry  +10-118 $full/assets/rocket.png $full/img/pic-2.png  $full/img/pic-ready.png
- echo "Picture ready. img/pic-ready.png"
+ echo "  sh:out Picture ready for upload."
 else
     $convert $full/img/pic-1.png -size 1080x1080  -gravity West   \
             -font $full/resources/Roboto/Roboto-Light.ttf -stroke  none -fill white -pointsize 40  -annotate +180-446   "$(head -1 $full/resources/author.txt)"  \
             -font $full/resources/Roboto/Roboto-Italic.ttf -stroke  none -fill white -pointsize 30  -annotate +180-398  "$(tail -n  +2 $full/resources/author.txt )"  \
             -gravity Center \
            -font $full/resources/Roboto/Roboto-Medium.ttf -stroke  none -fill white -pointsize 70  -annotate +1+20   "$(cat $full/resources/quote.txt)"  \
-           -font $full/resources/Roboto/Roboto-Thin.ttf -stroke  none -fill white -pointsize 30  -annotate +1+500   "@bookeklik_"  \
+           -font $full/resources/Roboto/Roboto-Thin.ttf -stroke  none -fill white -pointsize 30  -annotate +1+500   "@bot_keklik"  \
           $full/img/pic-2.png
-    echo 'Quote + author done.'
     $composite -geometry  +87+78 $full/assets/rocket.png $full/img/pic-2.png  $full/img/pic-ready.png
-    echo "Picture ready. img/pic-ready.png"
+    echo "  sh:out Picture ready for upload."
 fi
 
-echo 'Uploading..'
+echo '  sh:out Uploading..'
 php $full/php/uploadPhoto.php > $full/logs/instagram.log
-echo 'Uploaded.'
+echo '  sh:out Uploaded.'
